0caaa08a54caa2b6a37d0a548129360a227eb808
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-03-18 14:19:07 +0100

    A szabályok beépítjük a Validator-ba hibaüzenetekkel.

--- Kreata.Desktop/Validation/NameValidator.cs
+++ Kreata.Desktop/Validation/NameValidator.cs
@@ -12,6 +12,22 @@ namespace Kreta.Desktop.Validation
             {
                 string nameToValidate = (string)value;
                 NameValidationRules nvr = new NameValidationRules(nameToValidate);
+                if (nvr.IsNameShort)
+                {
+                    return new ValidationResult(false, "A név túl rövid!");
+                }
+                if (!nvr.IsFirstLetterUppercase)
+                {
+                    return new ValidationResult(false, "A név első betűje nagybetű kell legyen!");
+                }
+                if (!nvr.IsOtherLetterLowercase)
+                {
+                    return new ValidationResult(false, "Csak a név első betűje lehet nagybetű!");
+                }
+                if (!nvr.IsOnlyLetters)
+                {
+                    return new ValidationResult(false, "A név csak betűből állhat!");
+                }
             }
             return new ValidationResult(true, "");
         }
