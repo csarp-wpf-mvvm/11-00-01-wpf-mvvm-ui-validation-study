cb6f4abc07350c983a344b87a3b89de541c75ded
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-03-18 13:31:12 +0100

    Implementáljuk azt abastrakt osztályt

--- Kreata.Desktop/Validation/NameValidator.cs
+++ Kreata.Desktop/Validation/NameValidator.cs
@@ -1,8 +1,13 @@
-﻿using System.Windows.Controls;
+﻿using System.Globalization;
+using System.Windows.Controls;
 
 namespace Kreta.Desktop.Validation
 {
     public class NameValidator : ValidationRule
     {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
