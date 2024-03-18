3ff9ccc42c116bb319352f691202584313513b83
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-03-18 13:25:39 +0100

    Az osztály egy validálandó nevet kap a konstruktorban

--- Kreata.Desktop/Validation/ValidationRules/NameValidationRules.cs
+++ Kreata.Desktop/Validation/ValidationRules/NameValidationRules.cs
@@ -8,6 +8,10 @@ namespace Kreta.Desktop.Validation.ValidationRules
 {
     public class NameValidationRules
     {
-
+        private readonly string nameToValidate;
+        public NameValidationRules(string name)
+        {
+            this.nameToValidate = name;
+        }
     }
 }
